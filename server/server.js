e9be09b4f4178b207659e1865ae30acb861c41e0
Prabagaran_G <Prabagaran_G@Dellteam.com>
2022-10-22 01:40:05 +0530

    ch

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const jwt = require("_helpers/jwt");
 const errorHandler = require("_helpers/error-handler");
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
